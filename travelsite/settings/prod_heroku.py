cda2fbd29811e670ae70f88d5b82c51a93aeb919
daseulll <nldaseul@gmail.com>
2019-03-04 18:08:03 +0900

    Modified prod_heroku

--- travelsite/settings/prod_heroku.py
+++ travelsite/settings/prod_heroku.py
@@ -1,7 +1,8 @@
 from .base import * 
 
+DEBUG = False
+ALLOWED_HOSTS = ['travel-blog-site.herokuapp.com', '127.0.0.1']
+
 import django_heroku
 django_heroku.settings(locals())
 
-DEBUG = False
-ALLOWED_HOSTS = ['travel-blog-site.herokuapp.com', '127.0.0.1']
