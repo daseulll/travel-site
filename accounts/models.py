82662f01a415adbe76088a5c72b69c786119f703
daseulll <nldaseul@gmail.com>
2019-03-13 14:58:16 +0900

    Add Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    bio = models.TextField()
