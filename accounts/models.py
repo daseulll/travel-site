590a089c2a8b48d643e3909b5eabc9aba7d244ff
daseulll <nldaseul@gmail.com>
2019-03-13 15:52:56 +0900

    Create Profile objects for existing User.objects

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,14 @@
 from django.conf import settings
+from django.db.models.signals import post_save 
 from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
-    bio = models.TextField()
+    bio = models.TextField(blank=True)
+
+def on_post_save_for_user(sender, **kwargs):
+    if kwargs['created']:
+        user = kwargs['instance']
+
+post_save.connect(on_post_save_for_user, sender=settings.AUTH_USER_MODEL)
