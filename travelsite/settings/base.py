6c53d58cee1bb4fad7db44763df012439b04a112
daseulll <nldaseul@gmail.com>
2019-03-15 21:22:11 +0900

    Set amazon s3 settings

--- travelsite/settings/base.py
+++ travelsite/settings/base.py
@@ -20,16 +20,6 @@ BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-# from django.core.exceptions import ImproperlyConfigured
-
-# def get_env_varible(var_name):
-#     '''환경변수를 가져오거나 예외를 반환'''
-#     try:
-#         return os.environ[var_name]
-#     except KeyError:
-#         error_msg = "Set the {} environment variable".format(var_name)
-#         raise ImproperlyConfigured(error_msg)        
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
@@ -53,6 +43,7 @@ INSTALLED_APPS = [
 
     'debug_toolbar',
     'django_extensions',
+    'storages',
     
     'blog',
     'accounts',
@@ -126,18 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
 
 
 
--- travelsite/settings/prod_heroku.py
+++ travelsite/settings/prod_heroku.py
@@ -5,3 +5,19 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 import django_heroku
 django_heroku.settings(locals())
+
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
