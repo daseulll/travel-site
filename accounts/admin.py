9dd4839dc601f3bcc960a0c56d6b47a5e2d63469
daseulll <nldaseul@gmail.com>
2019-03-22 23:04:55 +0900

    Custom admin.site by adding UserDateJoinedFilter

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib import admin
+from .filter import UserDateJoinedFilter
 
-# Register your models here.
+
+admin.site.unregister(User)
+@admin.register(User)
+class UserAdmin(AuthUserAdmin):
+    list_display = ('username', 'email', 'is_staff')
+    list_filter = ('is_staff', 'is_active', 'date_joined', UserDateJoinedFilter)
